7232ba84975fc79894ba278a0d29c3946451b7b5
rohtanks <rohtanks@gmail.com>
2017-07-12 22:39:41 +0900

    관리자용 bootstrap 프레임 페이지 추가
    모든 관리자용 페이지 제일 하단에 include 시켜서 사용한다

---
+++ admin/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+              <p>Copyright &copy; prjCandle 2017</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="../js/bootstrap.min.js"></script>
+	
+<!-- Metis Menu Plugin JavaScript -->
+<script src="../js/metisMenu.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="../js/raphael.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="../js/sb-admin-2.js"></script>
