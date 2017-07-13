91017e96e41d8683e63ae0ffe1085576634f0cbe
rohtanks <rohtanks@gmail.com>
2017-07-13 15:06:29 +0900

    $domainName변수 수정
    개발 시 여러 호스트에서 사용하기 편하도록 SERVER변수 사용하여 수정
    주석 추가

--- config.php
+++ config.php
@@ -1,4 +1,29 @@
-<?php 
-$domainName = "http://localhost/";
+<?php
+// TODO HTTP_HOST는 사이트가 여러 환경에서 사용될 때 유용하다(로컬, 다른 로컬, 개발 서버 등) 보안에는 취약한 문제가 있으니 꼭 실제 도메인 이름을 사용할 것!!!
+$domainName = "http://".$_SERVER['HTTP_HOST']."/"; 
+
+/* $domains = array('domain.com', 'dev.domain.com', 'staging.domain.com', 'localhost');
+if (in_array($_SERVER['HTTP_HOST'], $domains))
+{
+	$domain = $_SERVER['HTTP_HOST'];
+}
+else
+{
+	$domain = 'localhost';
+} 이런 방법이나 */
+/* switch ($_SERVER['HTTP_HOST']) {
+	case 'domain.com':
+		$domain = 'domain.com';
+		break;
+	case 'dev.domain.com':
+		$domain = 'dev.domain.com';
+		break;
+	case 'staging.domain.com':
+		$domain = 'staging.domain.com';
+		break;
+	default:
+		$domain = 'local.domain';
+		break;
+} 이런 방법을 사용하면 괜찮을 듯*/
 
 ?>
