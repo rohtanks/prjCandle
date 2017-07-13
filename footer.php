fd8823930c49638cd093892007dad15da25fd248
rohtanks <rohtanks@gmail.com>
2017-07-13 15:06:58 +0900

    변경 사항 없음

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 text-center">
-              <p>Copyright &copy; prjCandle 2017</p>
-            </div>
-        </div>
-    </div>
-</footer>
+	<footer>
+	    <div class="container">
+	        <div class="row">
+	            <div class="col-lg-12 text-center">
+	              <p>Copyright &copy; prjCandle 2017</p>
+	            </div>
+	        </div>
+	    </div>
+	</footer>
 
 <!-- jQuery -->
 <script src="../js/jquery.js"></script>
