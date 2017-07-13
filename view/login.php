3fc5d4bceb34ceb0cabdb279bf326198810b32fb
rohtanks <rohtanks@gmail.com>
2017-07-13 21:36:52 +0900

    일반 사용자 로그인 페이지 스타일 수정

--- view/login.php
+++ view/login.php
@@ -18,33 +18,30 @@ if (isset($login_user)) {
 						<strong>로그인</strong>
 					</h2>
 					<hr>
-					<div class="col-md-4 col-md-offset-4">
-						<div class="login-panel panel panel-default">
-							<div class="panel-heading">
-								<h3 class="panel-title">아이디와 비밀번호를 입력하세요.</h3>
-							</div>
-							<div class="panel-body">
-								<form role="form" action="<?=$domainName?>prjcandle/member/loginOk.php"
-									method="post" accept-charset="utf-8">
-									<fieldset>
-										<div class="form-group">
-											<input type="text" name="mem_nickname" class="form-control" placeholder="아이디" autofocus="autofocus">
-										</div>
-										<div class="form-group">
-											<input type="password" name="mem_pw" class="form-control" placeholder="비밀번호">
-										</div>
-										<div class="form-group">
-											<input type="hidden" name="save" value="contact"> <!-- 아이디 저장 기능 사용 할까? -->
-											<button type="submit" class="btn btn-lg btn-primary btn-block">로그인</button>
-										</div>
-									</fieldset>
-								</form>
-							</div>
-							<!-- /.panel-body -->	
+					<div class="login-panel panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">아이디와 비밀번호를 입력하세요.</h3>
 						</div>
-						<!-- /.login-panel panel panel-default -->
+						<div class="panel-body">
+							<form role="form" action="<?=$domainName?>prjcandle/member/loginOk.php"
+								method="post" accept-charset="utf-8">
+								<fieldset>
+									<div class="form-group">
+										<input type="text" name="mem_nickname" class="form-control" placeholder="아이디" autofocus="autofocus">
+									</div>
+									<div class="form-group">
+										<input type="password" name="mem_pw" class="form-control" placeholder="비밀번호">
+									</div>
+									<div class="form-group">
+										<input type="hidden" name="save" value="contact"> <!-- 아이디 저장 기능 사용 할까? -->
+										<button type="submit" class="btn btn-lg btn-primary btn-block">로그인</button>
+									</div>
+								</fieldset>
+							</form>
+						</div>
+						<!-- /.panel-body -->	
 					</div>
-					<!-- /.col-md-4 col-md-offset-4 -->
+					<!-- /.login-panel panel panel-default -->
 				</div>
 				<!-- /.col-lg-12 -->
 			</div>
