ca07de86b98d1da4415077e17d0f365e0f399e06
rohtanks <rohtanks@gmail.com>
2017-07-13 21:49:11 +0900

    일반 사용자 글 쓰기 페이지 bootstrap 스타일 적용

--- view/write.php
+++ view/write.php
@@ -4,84 +4,140 @@ if(!isset($_SESSION)){
 }
 $login_user = $_SESSION['login_user'];
 
-include '../config.php';
 include '../dbConfig.php';
 include '../common.php';
 
 // mode 프로퍼티를 통해 글 쓰기인지 수정하기인지 알아본다
 $mode = $_GET['mode'];
 ?>
-<body>
-<div class="box" style="padding-left: 600px;">
-	<div><?php echo $login_user?></div>
-	<?php
-	// 글 쓰기
-	if ($mode == 'write') {
-	?>
-	<form action="../board/writeOk.php?mode=<?= $mode ?>" method="post"
-			accept-charset="utf-8">
-		<label>제목</label>
-		<input type="text" name="brd_title" /><br>
-		<label>분류</label>
-		<select name="brd_cate">
-			<option value="1">상품문의</option>
-			<option value="2">인생상담</option>
-			<option value="3">연예상담</option>
-		</select><br>
-		<label>내용</label>
-		<textarea name="brd_content"></textarea><br>
-		<label>업로드 파일</label>
-		<input type="button" name="brd_photo" value="사진 업로드"/><br>
-<!-- 		<input type="button" name="" value="파일 업로드"/><br> -->
-		<label>비밀글 여부</label>
-		<input type="checkbox" name="brd_secret" value="1"/>비밀글을 위한 체크
-		<div>
-			<p>
-				<button type="submit">작성완료</button><br>
-				<!-- 버튼 누를 시 자바스크립트 실행되어 이전 페이지로 돌아감 -->
-				<button onclick="history.back(); return false;">취소</button> <!-- IE에서 button태그에서 history.back()은 안먹힘 오동작,
-																				history.back()에 return false를 주면 정상 동작
-																				input태그는 또 먹힘 -->
-			</p>
+	<div class="container">
+		<div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                	<hr>
+                    <h2 class="intro-text text-center">
+                        <strong>글쓰기</strong>
+                    </h2>
+                    <hr>
+                    <div class="panel panel-default">
+						<div align="right"><?php echo $login_user?></div>
+						<div class="panel-body">
+							<div class="table-responsive">
+							<?php
+							// 글 쓰기
+							if ($mode == 'write') {
+							?>
+								<form role="form" action="../board/writeOk.php?mode=<?= $mode ?>" method="post"
+										accept-charset="utf-8">
+									<div class="form-group">	
+										<table class="table table-striped table-bordered table-hover">
+											<tbody>
+												<tr>
+													<th>제목</th>
+													<td><input type="text" name="brd_title" class="form-control" /></td>
+												</tr>
+												<tr>
+													<th>분류</th>
+													<td>
+														<select name="brd_cate" class="form-control">
+															<option value="1">상품문의</option>
+															<option value="2">인생상담</option>
+															<option value="3">연예상담</option>
+														</select>
+													</td>
+												</tr>
+												<tr>
+													<th>내용</th>
+													<td><textarea name="brd_content" class="form-control" rows="3"></textarea></td>
+												</tr>
+												<tr>
+													
+														<th>업로드 파일</th>
+														<td><input type="file" name="brd_photo" class="form-control" value="사진 업로드"/></td>
+												</tr>
+										<!-- 		<input type="button" name="" value="파일 업로드"/><br> -->
+												<tr>
+													<th>비밀글 여부</th>
+													<td><input type="checkbox" name="brd_secret" value="1"/> 비밀글을 위한 체크</td>
+												</tr>
+											</tbody>
+										</table>										
+									</div>
+									<!-- /.form-group -->
+									<div>
+										<button type="submit" class="btn btn-default">작성완료</button>
+										<!-- 버튼 누를 시 자바스크립트 실행되어 이전 페이지로 돌아감 -->
+										<button class="btn btn-default" onclick="history.back(); return false;">취소</button> <!-- IE에서 button태그에서 history.back()은 안먹힘 오동작,
+																											history.back()에 return false를 주면 정상 동작
+																											input태그는 또 먹힘 -->
+									</div>
+								</form>
+						<?php
+						// 수정하기
+						} else if ($mode == 'modify') {
+							$id = $_GET['id'];
+							$sql_select_modify = "SELECT * FROM board WHERE brd_id = '".$id."'";
+							$result = mysqli_query($conn, $sql_select_modify);
+							$row = mysqli_fetch_assoc($result);
+						?>
+							<form role="form" action="<?=$domainName?>prjcandle/board/writeOk.php?id=<?= $id ?>&mode=<?= $mode ?>" method="post"
+									accept-charset="utf-8">
+								<div class="form-group">	
+									<table class="table table-striped table-bordered table-hover">
+										<tbody>
+											<tr>
+												<th>제목</th>
+												<td><input type="text" name="brd_title" class="form-control" value="<?= $row['brd_title'] ?>" /></td>
+											</tr>
+											<tr>
+												<th>분류</th>
+												<td>
+													<select name="brd_cate" class="form-control"> <!-- TODO 가져온 값과 비교해서 selected --> 
+														<option value="1">상품문의</option>
+														<option value="2">인생상담</option>
+														<option value="3">연예상담</option>
+													</select>
+												</td>
+											</tr>
+											<tr>
+												<th>내용</th>
+												<td><textarea name="brd_content" class="form-control" rows="3"><?= $row['brd_content'] ?></textarea></td>
+											</tr>
+											<tr>
+												<th>업로드 파일</th>
+												<td><input type="file" name="brd_photo" class="form-control" value="사진 업로드"/></td>
+											</tr>
+										<!-- 		<input type="button" name="" value="파일 업로드"/><br> -->
+											<tr>
+												<th>비밀글 여부</th>
+												<td><input type="checkbox" name="brd_secret" value="1"/> 비밀글을 위한 체크</td>
+											</tr>
+										</tbody>
+									</table>										
+								</div>
+								<!-- /.form-group -->
+								<div>
+									<button type="submit" class="btn btn-default">작성완료</button>
+									<button class="btn btn-default" onclick="history.back(); return false;">취소</button>
+								</div>
+							</form>
+						<?php
+						}
+						?>
+						</div>
+						<!-- /.table-responsive -->
+					</div>
+					<!-- /.panel-body -->
+				</div>
+				<!-- /.panel panel-default -->
+            </div>
+            <!-- /.col-lg-6 -->
+			</div>
+			<!-- /.box -->
 		</div>
-	</form>
-	<?php
-	// 수정하기
-	} else if ($mode == 'modify') {
-		$id = $_GET['id'];
-		$sql_select_modify = "SELECT * FROM board WHERE brd_id = '".$id."'";
-		$result = mysqli_query($conn, $sql_select_modify);
-		$row = mysqli_fetch_assoc($result);
-	?>
-	<form action="<?=$domainName?>prjcandle/board/writeOk.php?id=<?= $id ?>&mode=<?= $mode ?>" method="post"
-			accept-charset="utf-8">
-		<label>제목</label>
-		<input type="text" name="brd_title" value="<?= $row['brd_title'] ?>" /><br>
-		<label>분류</label>
-		<select name="brd_cate">
-			<option value="1">상품문의</option>
-			<option value="2">인생상담</option>
-			<option value="3">연예상담</option>
-		</select><br>
-		<label>내용</label>
-		<textarea name="brd_content"><?= $row['brd_content'] ?></textarea><br>
-		<label>업로드 파일</label>
-		<input type="button" name="brd_photo" value="사진 업로드"/><br>
-<!-- 		<input type="button" name="" value="파일 업로드"/><br> -->
-		<label>비밀글 여부</label>
-		<input type="checkbox" name="brd_secret" value="1"/>비밀글을 위한 체크
-		<div>
-			<p>
-				<button type="submit">작성완료</button><br>
-				<!-- 버튼 누를 시 자바스크립트 실행되어 이전 페이지로 돌아감 -->
-				<button onclick="history.back(); return false;">취소</button>
-			</p>
-		</div>
-	</form>
-	<?php
-	}
-	?>
-</div>
+		<!-- /.row -->
+	</div>
+    <!-- /.container -->
 <?php
 include '../footer.php';
 ?>
